7536dadc7d0f6b67132066ed91bbf043c3b15c94
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-05-17 22:16:41 -0400

    local storage and cookie helpers

---
+++ client/helpers/auth.js
@@ -0,0 +1,65 @@
+import cookie from 'js-cookie'
+
+// set in cookie
+
+export const setCookie = (key, value) => {
+    if (process.browser) {
+        cookie.set(key, value, {
+            expires: 7
+        })
+    }
+}
+
+// remove from cookie
+export const removeCookie = (key) => {
+    if (process.browser) {
+        cookie.remove(key)
+    }
+}
+
+// get from cookie such as as stored token
+// will be userful when we need to make request to server with auth token
+
+export const getCookie = (key) => {
+    if (process.browser) {
+        return cookie.get(key)
+    }
+}
+
+// set in localstorage
+
+export const setLocalStorage = (key, value) => {
+    if (process.browser) {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
+
+// remove from localstorage
+
+export const removeLocalStorage = (key) => {
+    if (process.browser) {
+        localStorage.removeItem(key)
+    }
+}
+
+// authenticate by passing data to cookie and localstorage during signin
+
+export const authenticate = (response, next) => {
+    setCookie('token', response.data.token)
+    setLocalStorage('user', response.data.user)
+    next()
+}
+
+// accesss user info from localstorage
+export const isAuth = () => {
+    if (process.browser) {
+        const cookieChecked = getCookie('token')
+        if (cookieChecked) {
+            if (localStorage.getItem('user')) {
+                return JSON.parse(localStorage.getItem('user'))
+            } else {
+                return false
+            }
+        }
+    }
+}
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,88 @@
 import Layout from '../components/Layout';
+import { useState, useEffect } from 'react';
+import Link from 'next/link'
+import Router from 'next/router'
+import axios from 'axios';
+import { showSuccessMessage, showErrorMessage } from '../helpers/alerts';
+import { API } from '../config'
 
 const Login = () => {
+	const [
+		values,
+		setValues
+	] = useState({
+		email: '',
+		password: '',
+		buttonText: 'Login',
+		error: '',
+		success: ''
+	});
+
+	const { email, password, error, success, buttonText } = values;
+
+	const handleChange = (name) => (e) => {
+		setValues({ ...values, [name]: e.target.value, error: '', success: '', buttonText: 'Login' });
+		// console.log(e.target.value)
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setValues({ ...values, buttonText: 'Logging in' });
+		try {
+			const response = await axios.post(`${API}/login`, {
+				email,
+				password
+			});
+			console.log(response)
+		} catch (error) {
+			setValues({
+				...values,
+				buttonText: 'Login',
+				error: error.response.data.error
+			});
+		}
+	};
+
+	const loginForm = () => {
+		return (
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<input
+						onChange={handleChange('email')}
+						type="email"
+						className="form-control"
+						value={email}
+						placeholder="Type your email"
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						onChange={handleChange('password')}
+						type="password"
+						className="form-control"
+						value={password}
+						placeholder="Type your password"
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<button className="btn btn-outline-warning float-right">{buttonText}</button>
+				</div>
+			</form>
+		);
+	};
+
 	return (
-		<Layout>Login Page</Layout>
+		<Layout>
+			<div className="col-md-6 offset-md-3">
+				<h1>Login</h1>
+				<br />
+				{success && showSuccessMessage(success)}
+				{error && showErrorMessage(error)}
+				{loginForm()}
+			</div>
+		</Layout>
 	);
 };
 
