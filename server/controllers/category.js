55d5a5382a440ceb4605ddc62c5e0ea988440492
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-06-10 23:44:32 -0400

    allow upload to S3 using fs

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,47 +1,92 @@
-const Category = require('../models/category')
-const slugify = require('slugify')
+const Category = require('../models/category');
+const slugify = require('slugify');
+const formidable = require('formidable');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
 
+const s3 = new AWS.S3({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION
+});
 
 // create, list, read, update, remove
+// exports.create = (req, res) => {
+// 	const { name, content } = req.body;
+
+// 	const slug = slugify(name);
+// 	const image = {
+// 		url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
+// 		key: '123'
+// 	};
+
+// 	const category = new Category({ name, slug, image });
+
+// 	category.postedBy = req.user._id;
+
+// 	category.save((err, data) => {
+// 		if (err) {
+// 			console.log('Category create error', err);
+// 			return res.status(400).json({
+// 				error: 'Category create failed'
+// 			});
+// 		}
+// 		res.json(data);
+// 	});
+// };
 
 exports.create = (req, res) => {
-    const { name, content } = req.body
+	let form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				error: 'Image could not upload'
+			});
+		}
+		// console.table({err, fields, files})
+		const { name, content } = fields;
+        const { image } = files;
+        
+        const slug = slugify(name);
+		let category = new Category({ name, content, slug })
+		if (image.size > 2000000) {
+			return res.status(400).json({
+				error: 'Image should be less than 2mb'
+			});
+		}
 
-    const slug = slugify(name)
-    const image = {
-        url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
-        key: '123'
-    }
+		// upload image to s3
+		const params = {
+			Bucket: 'juwami-react-node-aws',
+			Key: `category/${uuidv4()}`,
+			Body: fs.readFileSync(image.path),
+			ACL: 'public-read',
+			ContentType: 'image/jpg'
+		} 
 
+		s3.upload(params, (err, data) => {
+			if (err) res.status(400).json({ error: 'Upload to S3 failed' })
+			console.log('AWS Upload Response Data', data)
+			category.image.url = data.Location
+			category.image.key = data.key
 
-    const category = new Category({ name, slug, image })
+			// save to db
+			category.save((err, success) => {
+				if (err) {
+					console.log(err)
+					res.status(400).json({ error: 'Duplicate content' })
+				}
+				return res.json(success)
+			})
+		})
+	});
+};
 
-    category.postedBy = req.user._id
+exports.list = (req, res) => {};
 
-    category.save((err, data) => {
-        if (err) {
-            console.log('Category create error', err)
-            return res.status(400).json({
-                error: 'Category create failed'
-            })
-        }
-        res.json(data)
-    })
+exports.read = (req, res) => {};
 
-}
+exports.update = (req, res) => {};
 
-exports.list = (req, res) => {
-    
-}
-
-exports.read = (req, res) => {
-    
-}
-
-exports.update = (req, res) => {
-    
-}
-
-exports.remove = (req, res) => {
-    
-}
+exports.remove = (req, res) => {};
