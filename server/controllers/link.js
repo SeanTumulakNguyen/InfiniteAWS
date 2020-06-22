acc9ca0a07370a7d14cb43a2f7932f752a0df28a
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-06-22 19:45:54 -0400

    link routes and validators

---
+++ server/controllers/link.js
@@ -0,0 +1,25 @@
+const Link = require('../models/link')
+const slugify = require('slugify')
+
+// create, list, read, update, remove
+exports.create = (req, res) => {
+    const { title, url, categories, type, medium } = req.body
+    // console.table({title, url, categories, type, medium})
+
+}
+
+exports.list = (req, res) => {
+    
+}
+
+exports.read = (req, res) => {
+    
+}
+
+exports.update = (req, res) => {
+    
+}
+
+exports.remove = (req, res) => {
+    
+}
