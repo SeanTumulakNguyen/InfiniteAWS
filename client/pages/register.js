d06311aeb827dc1644efbd86cd3ddf73807567a3
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-07-08 20:04:42 -0400

    add categories to register frontend

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,29 +1,66 @@
 import Layout from '../components/Layout';
 import { useState, useEffect } from 'react';
-import Router from 'next/router'
+import Router from 'next/router';
 import axios from 'axios';
 import { showSuccessMessage, showErrorMessage } from '../helpers/alerts';
-import { API } from '../config'
-import { isAuth } from '../helpers/auth'
+import { API } from '../config';
+import { isAuth } from '../helpers/auth';
 
 const Register = () => {
-	const [
-		values,
-		setValues
-	] = useState({
+	const [ values, setValues ] = useState({
 		name: '',
 		email: '',
 		password: '',
 		buttonText: 'Register',
 		error: '',
-		success: ''
+		success: '',
+		loadedCategories: [],
+		categories: []
 	});
 
-	useEffect(() => {
-		isAuth() && Router.push('/')
-	}, [])
+	const { name, email, password, error, success, buttonText, loadedCategories, categories } = values;
 
-	const { name, email, password, error, success, buttonText } = values;
+	useEffect(() => {
+		isAuth() && Router.push('/');
+	}, []);
+
+	useEffect(() => {
+		loadCategories();
+	}, []);
+
+	const loadCategories = async () => {
+		const response = await axios.get(`${API}/categories`);
+		setValues({ ...values, loadedCategories: response.data });
+	};
+
+	const handleToggle = (c) => () => {
+		// return the first index or -1
+		const clickedCategory = categories.indexOf(c);
+		const all = [ ...categories ];
+
+		if (clickedCategory === -1) {
+			all.push(c);
+		} else {
+			all.splice(clickedCategory, 1);
+		}
+
+		// console.log('All >> categories', all)
+		setValues({ ...values, categories: all, success: '', error: '' });
+	};
+
+	const showCategories = () => {
+		return (
+			loadedCategories &&
+			loadedCategories.map((c, i) => {
+				return (
+					<li className="list-unstyled" key={i}>
+						<input type="checkbox" onChange={handleToggle(c._id)} className="mr-2" />
+						<label className="form-check-label">{c.name}</label>
+					</li>
+				);
+			})
+		);
+	};
 
 	const handleChange = (name) => (e) => {
 		setValues({ ...values, [name]: e.target.value, error: '', success: '', buttonText: 'Register' });
@@ -37,7 +74,8 @@ const Register = () => {
 			const response = await axios.post(`${API}/register`, {
 				name,
 				email,
-				password
+				password,
+				categories
 			});
 			setValues({
 				...values,
@@ -55,35 +93,6 @@ const Register = () => {
 			});
 		}
 	};
-
-	// const handleSubmit = (e) => {
-	// 	e.preventDefault();
-	// 	setValues({ ...values, buttonText: 'Registering' });
-	// 	// console.table({ name, email, password });
-	// 	axios
-	// 		.post(`http://localhost:8000/api/register`, {
-	// 			name,
-	// 			email,
-	// 			password
-	// 		})
-	// 		.then((response) => {
-	// 			setValues({
-	// 				...values,
-	// 				name: '',
-	// 				email: '',
-	// 				password: '',
-	// 				buttonText: 'Submitted',
-	// 				success: response.data.message
-	// 			});
-	// 		})
-	// 		.catch((error) => {
-	// 			setValues({
-	// 				...values,
-	// 				buttonText: 'Register',
-	// 				error: error.response.data.error
-	// 			});
-	// 		});
-	// };
 
 	const registerForm = () => {
 		return (
@@ -117,6 +126,10 @@ const Register = () => {
 						placeholder="Type your password"
 						required
 					/>
+				</div>
+				<div className="form-group">
+					<label className="text-muted ml-4">Category</label>
+					<ul style={{ maxHeight: '100px', overflowY: 'scroll' }}>{showCategories()}</ul>
 				</div>
 				<div className="form-group">
 					<button className="btn btn-outline-warning float-right">{buttonText}</button>
