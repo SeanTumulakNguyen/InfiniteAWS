627064988510ca3703bc701781d826470569a035
sean.nguyenf@yahoo.com <sean.nguyenf@yahoo.com>
2020-07-11 11:44:34 -0400

    add meta for seo

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -1,9 +1,10 @@
 import Layout from '../../components/Layout';
 import Link from 'next/link';
+import Head from 'next/head';
 import axios from 'axios';
-import { API } from '../../config';
+import { API, APP_NAME } from '../../config';
 import renderHTML from 'react-render-html';
-import { useState } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import moment from 'moment';
 import InfiniteScroll from 'react-infinite-scroller';
 
@@ -12,10 +13,67 @@ const Links = ({ query, category, links, totalLinks, linksLimit, linkSkip }) => 
 	const [ limit, setLimit ] = useState(linksLimit);
 	const [ skip, setSkip ] = useState(0);
 	const [ size, setSize ] = useState(totalLinks);
+	const [ popular, setPopular ] = useState([]);
+
+	const stripHTML = (data) => data.replace(/<\/?[^>]+(>|$)/g, '');
+
+	const head = () => (
+		<Head>
+			<title>
+				{category.name} | {APP_NAME}
+			</title>
+			<meta name="description" content={stripHTML(category.content.substring(0, 160))} />
+			<meta property="og:title" content={category.name} />
+			<meta property="og:description" ccontent={stripHTML(category.content.substring(0, 160))} />
+			<meta property="og:image" content={category.image.url} />
+			<meta property="og:image:secure_url" content={category.image.url} />
+		</Head>
+	);
+
+	useEffect(() => {
+		loadPopular();
+	}, []);
+
+	const loadPopular = async () => {
+		const response = await axios.get(`${API}/link/popular/${category.slug}`);
+		setPopular(response.data);
+	};
+
+	const listOfPopularLinks = () =>
+		popular.map((l, i) => (
+			<div key={i} className="row alert alert-secondary p-2">
+				<div className="col-md-8" onClick={() => handleClick(l._id)}>
+					<a href={l.url} target="_blank">
+						<h5 className="pt-2">{l.title}</h5>
+						<h6 className="pt-2 text-danger" style={{ fontSize: '12px' }}>
+							{l.url}
+						</h6>
+					</a>
+				</div>
+
+				<div className="col-md-4 pt-2">
+					<span className="pull-right">
+						{moment(l.createdAt).fromNow()} by {l.postedBy.name}
+					</span>
+				</div>
+
+				<div className="col-md-12">
+					<span className="badge text-dark">
+						{l.type} {l.medium}
+					</span>
+					{l.categories.map((c, i) => (
+						<span key={i} className="badge text-success">
+							{c.name}
+						</span>
+					))}
+					<span className="badge text-secondary pull-right">{l.clicks} clicks</span>
+				</div>
+			</div>
+		));
 
 	const handleClick = async (linkId) => {
 		const response = await axios.put(`${API}/click-count`, { linkId });
-		loadUpdatedLinks();
+		loadPopular();
 	};
 
 	const loadUpdatedLinks = async () => {
@@ -69,54 +127,41 @@ const Links = ({ query, category, links, totalLinks, linksLimit, linkSkip }) => 
 		setSkip(toSkip);
 	};
 
-	// const loadMoreButton = () => {
-	// 	return (
-	// 		size > 0 &&
-	// 		size >= limit && (
-	// 			<button onClick={loadMore} className="btn btn-outline-primary btn-lg">
-	// 				Load more
-	// 			</button>
-	// 		)
-	// 	);
-	// };
-
 	return (
-		<Layout>
-			<div className="row">
-				<div className="col-md-8">
-					<h1 className="display-4 font-weight-bold">{category.name} - URL/Links</h1>
-					<div className="lead alert alert-secondary pt-4">{renderHTML(category.content || '')}</div>
-				</div>
-				<div className="col-md-4">
-					<img src={category.image.url} alt={category.name} style={{ width: 'auto', maxHeight: '200px' }} />
-				</div>
-			</div>
-			<br />
-			{/* <div className="row">
-				<div className="col-md-8">{listOfLinks()}</div>
-				<div className="col-md-4">
-					<h2 className="lead">Most popular in {category.name}</h2>
-					<p>show popular links</p>
-				</div>
-			</div> */}
-
-			{/* <div className="text center pb-5 pt-4">{loadMoreButton()}</div> */}
-
-			<InfiniteScroll
-				pageStart={0}
-				loadMore={loadMore}
-				hasMore={size > 0 && size >= limit}
-				loader={<img key={0} src="/static/images/gifLoader.gif" alt="loading" />}
-			>
+		<Fragment>
+			{head()}
+			<Layout>
 				<div className="row">
-					<div className="col-md-8">{listOfLinks()}</div>
+					<div className="col-md-8">
+						<h1 className="display-4 font-weight-bold">{category.name} - URL/Links</h1>
+						<div className="lead alert alert-secondary pt-4">{renderHTML(category.content || '')}</div>
+					</div>
 					<div className="col-md-4">
-						<h2 className="lead">Most popular in {category.name}</h2>
-						<p>show popular links</p>
+						<img
+							src={category.image.url}
+							alt={category.name}
+							style={{ width: 'auto', maxHeight: '200px' }}
+						/>
 					</div>
 				</div>
-			</InfiniteScroll>
-		</Layout>
+				<br />
+
+				<InfiniteScroll
+					pageStart={0}
+					loadMore={loadMore}
+					hasMore={size > 0 && size >= limit}
+					loader={<img key={0} src="/static/images/gifLoader.gif" alt="loading" />}
+				>
+					<div className="row">
+						<div className="col-md-8">{listOfLinks()}</div>
+						<div className="col-md-4">
+							<h2 className="lead">Most popular in {category.name}</h2>
+							<div className="p-3">{listOfPopularLinks()}</div>
+						</div>
+					</div>
+				</InfiniteScroll>
+			</Layout>
+		</Fragment>
 	);
 };
 
